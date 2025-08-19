e2b433ad8bafbe96150d95529f4a70d1b73530d7
Joao Lucas <jldecristo@gmail.com>
2025-08-19 20:57:07 -0300

    Bee1008 resolvido

---
+++ src/Bee1008.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bee1008 {
+
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int hour = sc.nextInt();
+        double amont = sc.nextDouble();
+
+        double salary = (double) hour * amont;
+
+        System.out.println("NUMBER = " + number);
+        System.out.printf("SALARY = %.2f%n", salary);
+
+        sc.close();
+    }
+
+}
