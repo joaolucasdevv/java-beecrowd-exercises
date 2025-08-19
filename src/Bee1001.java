bb188560e5e84222646e97d515bae7ddf5fcad8f
Joao Lucas <jldecristo@gmail.com>
2025-08-18 22:31:30 -0300

    ambiente

--- src/Bee1001.java
+++ src/Bee1001.java
@@ -1,7 +1,14 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Bee1001 {
 
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
 
+
+        sc.close();
     }
 
 }
