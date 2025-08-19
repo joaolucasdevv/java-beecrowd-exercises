9a67668823c83bd110acc2270be6960284e4c621
Joao Lucas <jldecristo@gmail.com>
2025-08-19 20:40:41 -0300

    Bee1007 resolvido

--- src/Bee1007.java
+++ src/Bee1007.java
@@ -7,8 +7,14 @@ public class Bee1007 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
 
+        int diferenca = a * b - c * d;
 
+        System.out.println("DIFERENCA = " + diferenca);
 
         sc.close();
     }
