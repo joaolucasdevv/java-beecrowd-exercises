c89f7d27b8b5c3330c12d17a3dac50d51c738c24
Joao Lucas <jldecristo@gmail.com>
2025-08-20 19:09:37 -0300

    Bee1017 resolvido

--- src/Bee1017.java
+++ src/Bee1017.java
@@ -7,9 +7,12 @@ public class Bee1017 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int time = sc.nextInt();
+        int speed = sc.nextInt();
 
+        double liters = (double) speed * time / 12.0;
 
-
+        System.out.printf("%.3f%n", liters);
 
         sc.close();
     }
