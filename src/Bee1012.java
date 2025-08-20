b3416598ece124436ac7f085c57cfabdd3b4b8c2
Joao Lucas <jldecristo@gmail.com>
2025-08-19 22:30:17 -0300

    prints

--- src/Bee1012.java
+++ src/Bee1012.java
@@ -17,6 +17,12 @@ public class Bee1012 {
         double quadrado = Math.pow(b, 2);
         double retangulo = a * b;
 
+        System.out.printf("TRIANGULO: %.3f%n", triangulo);
+        System.out.printf("CIRCULO: %.3f%n", cirulo);
+        System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+        System.out.printf("QUADRADO: %.3f%n", quadrado);
+        System.out.printf("RETANGULO: %.3f%n", retangulo);
+
         sc.close();
     }
 
