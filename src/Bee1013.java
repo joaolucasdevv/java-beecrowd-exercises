f6cb28f878e7e1bba2bbc91f967cab7b09f6fdc2
Joao Lucas <jldecristo@gmail.com>
2025-08-19 22:55:49 -0300

    teste do Bee1013

--- src/Bee1013.java
+++ src/Bee1013.java
@@ -7,7 +7,19 @@ public class Bee1013 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
+        if (a > b && a > c) {
+            System.out.println(a + " eh o maior");
+        }
+        else if (b > c) {
+            System.out.println(b + " eh o maior");
+        }
+        else {
+            System.out.println(c + " eh o maior");
+        }
 
         sc.close();
     }
