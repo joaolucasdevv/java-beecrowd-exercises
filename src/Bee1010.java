7c7a8a8e8972c68f35c959e2b466d861a8ac6d62
Joao Lucas <jldecristo@gmail.com>
2025-08-19 21:17:10 -0300

    Bee1010

--- src/Bee1010.java
+++ src/Bee1010.java
@@ -7,9 +7,17 @@ public class Bee1010 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int cod1 = sc.nextInt();
+        int num1 = sc.nextInt();
+        double price1 = sc.nextDouble();
 
+        int cod2 = sc.nextInt();
+        int num2 = sc.nextInt();
+        double price2 = sc.nextDouble();
 
+        double amount = num1 * price1 + num2 * price2;
 
+        System.out.printf("VALOR A PAGAR: R$ %.2f%n", amount);
 
         sc.close();
     }
