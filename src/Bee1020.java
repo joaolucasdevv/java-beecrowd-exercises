d3d31f1da387038bb3a71885870113d83dff3ad3
Joao Lucas <jldecristo@gmail.com>
2025-08-20 19:19:18 -0300

    Bee1020

--- src/Bee1020.java
+++ src/Bee1020.java
@@ -1,14 +1,14 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Bee1018 {
+public class Bee1020 {
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-        int money = sc.nextInt();
-        
+
+
 
 
         sc.close();
