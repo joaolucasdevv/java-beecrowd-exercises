52bf7eff93d79949d5c48c0bdb4ecb02a110d60d
Joao Lucas <jldecristo@gmail.com>
2025-08-20 22:29:40 -0300

    Bee1020

--- src/Bee1020.java
+++ src/Bee1020.java
@@ -7,6 +7,18 @@ public class Bee1020 {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        int days = sc.nextInt();
+
+        int year = days / 365;
+        int resto = days % 365;
+
+        int month = resto / 12;
+        resto = resto % 12;
+
+        int daysMonth = resto;
+
+        System.out.println(year + " ano(s)");
+        System.out.println(month + " ");
 
 
 
